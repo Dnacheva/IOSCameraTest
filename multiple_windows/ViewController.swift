c80fdfc86554a14fd876c8c0575d59c40a6418d2
akharag <akharag@nevada.unr.edu>
2019-02-12 12:03:37 -0800

    Initial Commit

---
+++ multiple_windows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiple_windows
+//
+//  Created by Masoud Sasha on 2/12/19.
+//  Copyright © 2019 Masoud Sasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ multiple_windowsTests/multiple_windowsTests.swift
@@ -0,0 +1,34 @@
+//
+//  multiple_windowsTests.swift
+//  multiple_windowsTests
+//
+//  Created by Masoud Sasha on 2/12/19.
+//  Copyright © 2019 Masoud Sasha. All rights reserved.
+//
+
+import XCTest
+@testable import multiple_windows
+
+class multiple_windowsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

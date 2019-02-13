2e065dc65e1e68d144c0d3d779673c2fc582aeeb
Alexander Kharag <akharag@nevada.unr.edu>
2019-02-12 16:03:53 -0800

    Test Camera and Record Results/Errors

--- multiple_windows/ViewController.swift
+++ multiple_windows/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     let captureSession = AVCaptureSession()
     var previewLayer:CALayer!
     
     var captureDevice:AVCaptureDevice!
     
+    var takePhoto = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        prepareCamera()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareCamera()
+
     }
 
     func prepareCamera() {
@@ -55,6 +61,64 @@ class ViewController: UIViewController {
         }
     
         captureSession.commitConfiguration()
+        
+        let queue = DispatchQueue(label: "com.MasoudSasha.captureQueue")
+        dataOutput.setSampleBufferDelegate(self, queue: queue)
     }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        takePhoto = true
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        
+        if takePhoto {
+            takePhoto = false
+            
+            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
+                
+                let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
+                
+                photoVC.takenPhoto = image
+                
+                DispatchQueue.main.async {
+                    self.present(photoVC, animated: true, completion: {
+                        self.stopCaptureSession()
+                    })
+                    
+                }
+            }
+            
+            
+        }
+    }
+    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+            
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    func stopCaptureSession () {
+        self.captureSession.stopRunning()
+        
+        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
+            for input in inputs {
+                self.captureSession.removeInput(input)
+            }
+        }
+        
+    }
+    
+    
 }
 
